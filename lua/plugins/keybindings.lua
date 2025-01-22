14c48a55032dc5474e9eacca5bea10bd31dbcf88
iljuhenson <ilyapavluk24@gmail.com>
2025-01-22 15:42:44 +0100

    Add paste and yank from clipboard functionalities

---
+++ lua/plugins/keybindings.lua
@@ -0,0 +1,40 @@
+return {
+	{
+		"folke/which-key.nvim",
+		event = "VeryLazy",
+		opts = {
+			-- your configuration comes here
+			-- or leave it empty to use the default settings
+			-- refer to the configuration section below
+		},
+		keys = {
+			{
+				"<leader>?",
+				function()
+					require("which-key").show({ global = false })
+				end,
+				desc = "Buffer Local Keymaps (which-key)",
+			},
+			{
+				'<leader>fy',
+				function ()
+					local current_buffer_relative_path = vim.fn.expand("%:.")
+					vim.fn.setreg("+", current_buffer_relative_path)
+				end,
+				desc = "Copy relative path of file in a buffer",
+			},
+			{
+				"<leader>y",
+				'"+y',
+				mode = "v",
+				desc = "Copy to clipboard",
+			},
+			{
+				"<leader>p",
+				'"+p',
+				mode = "v",
+				desc = "Paste from clipboard",
+			},
+		},
+	}
+}
